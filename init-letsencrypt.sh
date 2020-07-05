05ddfe75460c0fc30354e3bf51af1a05cf3334b3
emielvanderhoek <ehoek@openaware.eu>
2020-07-05 16:19:12 +0200

    simple 2 hosts

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(host-60.polkasource.io node-60.polkasource.io)
+domains=(host-60.polkasource.io)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ehoek@openaware.eu" # Adding a valid address is strongly recommended
